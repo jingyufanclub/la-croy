b213b4651cb860ec4dc0f28a9bea8d97648b1c11
deaundrapeek <jingyufanclub@gmail.com>
2017-05-12 01:44:31 -0400

    refresh page generates new pattern

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-$(document).ready(function() {
+window.onload = function() {
 
+  const bgImages = ['avocado', 'berry', 'cuke', 'flowers', 'grapefruit', 'peony', 'plain', 'pomelo', 'tangerine']
 
-//document.getElementsByTagName('body')
+  function randomize(array) {
+    let length = Math.floor(Math.random() * (array.length - 3 + 1)) + 3;
+    console.log(length);
+    let randomBgImages = [];
+    for (let i = 0; i < length; i++){
+      image = array.splice(Math.floor(Math.random() * array.length), 1);
+    console.log('url("images/' + image.toString() + '.png")')
+    randomBgImages.push('url("images/' + image.toString() + '.png")')
+    }
+    return randomBgImages.toString()
+  }
+  document.body.style.backgroundImage = randomize(bgImages);
 }
