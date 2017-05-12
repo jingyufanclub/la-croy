dcce029a9a40c4ce1f6661679e50a833b0a4e578
deaundrapeek <jingyufanclub@gmail.com>
2017-05-12 02:50:33 -0400

    use while loop and better var names

--- js/script.js
+++ js/script.js
@@ -3,16 +3,33 @@ window.onload = function() {
   const bgImages = ['avocado', 'berry', 'cuke', 'flowers', 'grapefruit', 'peony', 'plain', 'pomelo', 'tangerine']
 
   function randomize(array) {
+    // random int between two values, inclusive, i.e. minimum 3, maximum number of items in array: Math.floor(Math.random() * (max - min + 1)) + min
     let length = Math.floor(Math.random() * (array.length - 3 + 1)) + 3;
     console.log(length);
+
     let randomBgImages = [];
-    for (let i = 0; i < length; i++){
-      image = array.splice(Math.floor(Math.random() * array.length), 1);
-    console.log('url("images/' + image.toString() + '.png")')
-    randomBgImages.push('url("images/' + image.toString() + '.png")')
+
+    // iterate randomly selected number of times and each time randomly select an item to remove by splicing and put that item into an array
+    while (length) {
+      imageName = array.splice(Math.floor(Math.random() * array.length), 1);
+      console.log('url("images/' + imageName.toString() + '.png")');
+      randomBgImages.push('url("images/' + imageName.toString() + '.png")');
+      length--
     }
-    console.log(randomBgImages.toString())
-    return randomBgImages.toString()
+    console.log(randomBgImages.toString());
+    return randomBgImages.toString();
   }
   document.body.style.backgroundImage = randomize(bgImages);
 }
+
+/* try implement with Fisher-Yates shuffle algorithm for randomness
+function shuffle(array) {
+  var m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+}
+*/
