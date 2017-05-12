32a72653092467ab2c5aac04cdf75c4389791e15
deaundrapeek <jingyufanclub@gmail.com>
2017-05-12 13:46:11 -0400

    edit splice js method and keep it here for reference

--- js/splice.js
+++ js/splice.js
@@ -1,23 +1,20 @@
 window.onload = function() {
 
-  const bgImages = ['avocado', 'berry', 'cuke', 'flowers', 'grapefruit', 'peony', 'plain', 'pomelo', 'tangerine']
+  const images = ['avocado', 'berry', 'cuke', 'flowers', 'grapefruit', 'peony', 'plain', 'pomelo', 'tangerine']
 
   function randomize(array) {
-    // random int between two values, inclusive, i.e. minimum 3, maximum number of items in array: Math.floor(Math.random() * (max - min + 1)) + min
-    let length = Math.floor(Math.random() * (array.length - 3 + 1)) + 3;
-    console.log(length);
+    // Choose random integer between two values, inclusive, i.e. minimum 4, maximum number of items in array: Math.floor(Math.random() * (max - min + 1)) + min
+    let n = Math.floor(Math.random() * (array.length - 4 + 1)) + 4;
 
-    let randomBgImages = [];
+    let randomizedImages = [];
 
-    // iterate randomly selected number of times and each time randomly select an item to remove by splicing and put that item into an array
-    while (length) {
-      imageName = array.splice(Math.floor(Math.random() * array.length), 1);
-      console.log('url("images/' + imageName.toString() + '.png")');
-      randomBgImages.push('url("images/' + imageName.toString() + '.png")');
-      length--
+    // Iterate n times and randomly select an item to remove by splicing and put that item into a new array
+    while (n) {
+      image = array.splice(Math.floor(Math.random() * array.length), 1);
+      randomizedImages.push("url('images/" + image.toString() + ".png')");
+      n--
     }
-    console.log(randomBgImages.toString());
-    return randomBgImages.toString();
+    return randomizedImages.toString();
   }
-  document.body.style.backgroundImage = randomize(bgImages);
+  document.body.style.backgroundImage = randomize(images);
 }
